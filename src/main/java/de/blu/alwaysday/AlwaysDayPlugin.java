7fd2ef54f79e4cae81f3608514677a9ca23f98ef
Bluplayz <bluplayz98@gmail.com>
2021-05-19 14:22:40 +0200

    changed task to gamerule

--- src/main/java/de/blu/alwaysday/AlwaysDayPlugin.java
+++ src/main/java/de/blu/alwaysday/AlwaysDayPlugin.java
@@ -1,21 +1,33 @@
 package de.blu.alwaysday;
 
 import org.bukkit.Bukkit;
+import org.bukkit.GameRule;
 import org.bukkit.World;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.world.WorldLoadEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class AlwaysDayPlugin extends JavaPlugin {
-
-  private static final int TICKS_REPEATING = 20 * 15;
+public final class AlwaysDayPlugin extends JavaPlugin implements Listener {
 
   @Override
   public void onEnable() {
-    Bukkit.getScheduler().scheduleSyncRepeatingTask(this, this::updateTime, 0, TICKS_REPEATING);
+    for (World world : Bukkit.getWorlds()) {
+      this.initWorld(world);
+    }
+
+    Bukkit.getPluginManager().registerEvents(this, this);
   }
 
-  private void updateTime() {
-    for (World world : Bukkit.getWorlds()) {
-      world.setTime(8000);
-    }
+  @EventHandler
+  public void onWorldLoad(WorldLoadEvent e) {
+    World world = e.getWorld();
+
+    this.initWorld(world);
+  }
+
+  private void initWorld(World world) {
+    world.setTime(8000);
+    world.setGameRule(GameRule.DO_DAYLIGHT_CYCLE, false);
   }
 }
